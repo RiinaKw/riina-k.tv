15d81b631b449482e4b61065e6f2c2186b3b5f4b
Riina K <riinak.tv@gmail.com>
2018-08-07 03:33:16 +0900

    error setting

--- public/index.php
+++ public/index.php
@@ -4,11 +4,13 @@ mb_language('ja');
 mb_internal_encoding('UTF-8');
 mb_regex_encoding('UTF-8');
 
-ini_set( 'display_errors', 1 );
-error_reporting(E_ALL);
-
 require_once('../internal/init.inc.php');
 $config =  new Config(__DIR__);
+
+if ($config->env == 'development') {
+	ini_set( 'display_errors', 1 );
+	error_reporting(E_ALL);
+}
 
 require_once($config->vendor_dir . '/autoload.php');
 
