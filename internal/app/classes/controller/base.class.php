e36223f949220d95c63e2e831883f0d64aa3f3a5
Riina K <riinak.tv@gmail.com>
2018-10-01 03:12:23 +0900

    URL refactoring 2

--- internal/app/classes/controller/base.class.php
+++ internal/app/classes/controller/base.class.php
@@ -10,12 +10,19 @@ class Controller_Base extends Controller {
 		
 		$path = $bootstrap->app_config_path('conf/riina-k.tv.conf');
 		$this->_config = parse_ini_file($path);
-		$this->root = $bootstrap->root_url;
 	} // function __before()
 	
 	protected function _get_config($key)
 	{
 		return $this->_config[$key];
-	}
+	} // function _get_config()
+	
+	protected function url_create($path)
+	{
+		global $bootstrap;
+		
+		$first = substr($path, 0, 1);
+		return $bootstrap->root_url . ($first == '/' ? $first : '') . $path;
+	} // function url_create()
 	
 } // class Controller_Base
