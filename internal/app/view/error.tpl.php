60140f932b5b2b0116f0eefd4be40726a9b21275
Riina K <riinak.tv@gmail.com>
2019-10-19 21:04:45 +0900

    Fix html rendering of exception

--- internal/app/view/error.tpl.php
+++ internal/app/view/error.tpl.php
@@ -2,16 +2,19 @@
 <html lang="ja">
 	<head>
 		<meta charset="utf-8" />
-		<title><?php echo $this->title; ?> - <?php echo $this->config['title_en']; ?></title>
+		<title><?php echo htmlspecialchars($this->title); ?></title>
 	</head>
 	<body>
 <?php
 	if ($bootstrap->env == 'development') {
 		var_dump($this);
 	}
+
+	$message = $this->message;
+	$pos = strpos($message, ' in');
+	$message = substr($message, 0, $pos);
 ?>
-		<h1><?php echo $this->title; ?></h1>
-		<p><?php echo $this->message; ?></p>
-		<pre><?php echo $this->trace; ?></pre>
+		<h1><?php echo htmlspecialchars($this->title); ?></h1>
+		<p><?php echo htmlspecialchars($message); ?></p>
 	</body>
 </html>
